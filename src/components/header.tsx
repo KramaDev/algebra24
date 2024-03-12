032181f790c6fa23b7ab10b25c982f9cde179be5
grafika10 <grafika10@ucione.local>
2024-03-12 20:45:44 +0100

    Header

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div>LOGO</div>
+        <div>NavBar</div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
