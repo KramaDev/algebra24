09efd7d6bd3e5737e562c54a61f64680e2a72962
Mario Kramarić <mario.kramaric13@gmail.com>
2024-03-13 20:32:18 +0100

    Header

---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <img className="header__logo" src="" alt="" />
+        <div className="header__nav">
+          <a className="link" href="">
+            ABOUT
+          </a>
+          <a className="link" href="">
+            CAREER
+          </a>
+          <a className="link" href="">
+            REACH OUT
+          </a>
+        </div>
+      </header>
+    </>
+  );
+};
+export default Header;
