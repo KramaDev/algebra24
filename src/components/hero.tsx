ad1cd03f3c1de21e612da5d667b59c8055d4b985
Mario Kramarić <mario.kramaric13@gmail.com>
2024-03-13 21:05:06 +0100

    fasfas

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,8 +3,8 @@ const Hero = () => {
     <>
       <div className="hero">
         <div className="hero__leftbox">
-          Here in Algebra you can learn or teach what you love, we have a bunch
-          of courses that you can attend, you can reach out anytime if you have
+          Here in ALGEBRA you can learn or teach what you love, we have a bunch
+          of COURSES that you can attend, you can reach out anytime if you have
           more questions, we will be happy to help anytime!
         </div>
 
