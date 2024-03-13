2ccb89a49134c0de108bf30b9bbdd810cd5d9304
Mario Kramarić <mario.kramaric13@gmail.com>
2024-03-13 20:47:11 +0100

    gfhjjdhf

---
+++ src/components/hero.tsx
@@ -0,0 +1,21 @@
+const Hero = () => {
+  return (
+    <>
+      <div className="hero">
+        <div className="hero__leftbox">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
+          consequuntur praesentium unde nemo magni laboriosam amet voluptatem
+          similique nihil dicta architecto beatae debitis consequatur eius,
+          aliquid saepe iure placeat dolorem.
+        </div>
+
+        <img
+          className="hero__rightbox"
+          src="https://source.unsplash.com/random/1000x1000/?developer"
+          alt="hero photo"
+        />
+      </div>
+    </>
+  );
+};
+export default Hero;
