622b1f3468dff8de160360696a5f515d1dbd1cd3
Mario Kramarić <mario.kramaric13@gmail.com>
2024-03-18 21:02:53 +0100

    PokeApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout";
-import Home from "./pages/home";
-import NoMatch from "./pages/no-match";
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
+      <Layout />
     </>
   );
 };
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,18 +1,105 @@
+import { useEffect, useState, MouseEvent } from "react";
+import Select from "react-select";
+
+type PokemonsResponseType = {
+  count: number;
+  next: string;
+  results: PokemonType[];
+};
+
+type PokemonType = {
+  name: string;
+  url: string;
+};
+
+type ActivePokemonType = {
+  sprites: {
+    front_default: string;
+  };
+};
+type ActivePokemonResponseType = ActivePokemonType;
+
+//ItemsPerPage
+type OptionType = {
+  value: string;
+  label: string;
+};
+
+const options: OptionType[] = [
+  { value: "10", label: "10 Pokemona" },
+  { value: "20", label: "20 Pokemona" },
+  { value: "100", label: "100 Pokemona" },
+];
+
+const defaultLimit = "10";
+
 const Hero = () => {
+  const [data, setData] = useState<PokemonType[]>([]);
+  const [activePokemon, setActivePokemon] = useState<
+    ActivePokemonType | undefined
+  >(undefined);
+
+  const getData = async (limit: string | undefined) => {
+    await fetch(
+      `https://pokeapi.co/api/v2/pokemon/?limit=${limit ? limit : defaultLimit}`
+    )
+      .then((data) => {
+        return data.json();
+      })
+      .then((res: PokemonsResponseType) => {
+        setData(res.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getPokemonData = async (pokemon: string) => {
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then((data) => {
+        return data.json();
+      })
+      .then((res: ActivePokemonResponseType) => {
+        setActivePokemon(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getData("10");
+  }, []);
+
   return (
     <>
       <div className="hero">
-        <div className="hero__leftbox">
-          Here in ALGEBRA you can learn or teach what you love, we have a bunch
-          of COURSES that you can attend, you can reach out anytime if you have
-          more questions, we will be happy to help anytime!
-        </div>
-
-        <img
-          className="hero__rightbox"
-          src="https://source.unsplash.com/random/1000x1000/?developer"
-          alt="hero photo"
+        <h1 className="hero__title">Pokemons</h1>
+        <Select
+          onChange={(e) => {
+            getData(e?.value);
+          }}
+          options={options}
         />
+        <div className="hero__items">
+          {data.map((pokemon) => {
+            return (
+              <button
+                onClick={(e: MouseEvent<HTMLButtonElement>) => {
+                  const element = e.target as HTMLButtonElement;
+                  const currentPokemon = element.innerHTML;
+                  getPokemonData(currentPokemon);
+                }}
+                key={pokemon.name}
+              >
+                {pokemon.name}
+              </button>
+            );
+          })}
+        </div>
+        <div>
+          <img src={activePokemon?.sprites.front_default} alt="" />
+        </div>
       </div>
     </>
   );
